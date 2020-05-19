f741816e5ba68e1598ab395c7eb0a413e335fea1
rxshadowztw <tom.wong3975@gmail.com>
2020-05-19 13:42:05 -0700

    Add CSS reset file

--- src/ms_react.js
+++ src/ms_react.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './css-reset.css';
 import './ms_styles.css';
 import {MinesweeperGameState, CELL_STATE, CELL_VALUE, GAME_STATE, DIFFICULTY} from './minesweeper_logic';
 
