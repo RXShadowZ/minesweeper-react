695788912d9e593260325f998f7e1e65ccf69a9d
rxshadowztw <tom.wong3975@gmail.com>
2020-06-10 22:30:23 -0700

    Updates

---
+++ precache-manifest.088173e4993f75d6ec89bd8438feb215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41724b42b1fa94fd027266a47461b7b1",
+    "url": "/minesweeper-react/index.html"
+  },
+  {
+    "revision": "4c76afc02509912796d1",
+    "url": "/minesweeper-react/static/css/main.500e9639.chunk.css"
+  },
+  {
+    "revision": "a67fe74844cc426874d8",
+    "url": "/minesweeper-react/static/js/2.fa10ed0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper-react/static/js/2.fa10ed0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c76afc02509912796d1",
+    "url": "/minesweeper-react/static/js/main.4ecd91a3.chunk.js"
+  },
+  {
+    "revision": "6acdde21d2791114e935",
+    "url": "/minesweeper-react/static/js/runtime-main.85a929b5.js"
+  }
+]);
